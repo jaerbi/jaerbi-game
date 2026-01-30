cebb92e7a358c98e17e524b078c0516ad1926e79
jaerbi <jaerbi42@gmail.com>
2026-01-30 14:02:42 +0200

    version 0.5.1

--- src/app/app-game.ts
+++ src/app/app-game.ts
@@ -148,7 +148,7 @@ export class AppGame {
     }
 
     private async executeAutoMove() {
-        await new Promise(res => setTimeout(res, 500));
+        await new Promise(res => setTimeout(res, 300));
         if (!this.isAutoBattleActive()) return;
         if (this.gameEngine.gameStatus() !== 'playing') return;
         if (!this.gameEngine.isPlayerTurn()) return;
